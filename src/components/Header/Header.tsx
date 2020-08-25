51e58091cd6854368da8696e5a82edf3b184fa90
Matan Kotler-Berkowitz <205matan@gmail.com>
2020-08-25 19:46:44 -0400

    Change app logo

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -2,14 +2,19 @@ import React from 'react';
 import classNames from 'classnames';
 import { Link } from 'react-router-dom';
 
+// import { ReactComponent as Logo } from './logo.svg';
+import logo from './logo.png';
 import style from './header.module.css'
+
 
 const Header = () => (
   <header className={style['header']}>
     <Link to="/" className={style['home-link']}>
-      <i className={classNames('far fa-frown', style['logo-frown'])} />
-      <i className={classNames('fas fa-smile', style['logo-smile'])} />
       Kavanapp
+      {/* <i className={classNames('far fa-frown', style['logo-frown'])} />
+      <i className={classNames('fas fa-smile', style['logo-smile'])} /> */}
+      {/* <Logo className={style['logo']} color="white" /> */}
+      <img src={logo} className={style['logo']} alt="" />
     </Link>
   </header>
 )
