88fff9d83e1165b33bc9451471f314d6c9a3626e
Matan Kotler-Berkowitz <205matan@gmail.com>
2020-08-25 23:17:53 -0400

    Show loading indicator on form submit

--- src/components/Rating/RateFormSubmit.tsx
+++ src/components/Rating/RateFormSubmit.tsx
@@ -1,8 +1,11 @@
 import React, { useState } from 'react';
 
-import style from './rating.module.css'
-import { FormResponses } from './RateForm';
 import { getAllPrayers, setPrayer, addPrayer } from 'src/api/firebase';
+import Loading from '../Loading/Loading';
+
+import { FormResponses } from './RateForm';
+
+import style from './rating.module.css'
 
 interface RateFormSubmitProps {
   formResponses: FormResponses;
@@ -46,28 +49,63 @@ function getResponseCount(responses: FormResponses) {
   return Object.values(responses).filter(response => response.length).length;
 }
 
+enum SubmitStatus { NotSubmitted, Submitting, Submitted };
+
 const RateFormSubmit = (props: RateFormSubmitProps) => {
 
   const { formResponses } = props;
   const [age, setAge] = useState<number>();
+  const [submitStatus, setSubmitStatus] = useState<SubmitStatus>(SubmitStatus.NotSubmitted);
 
   const responseCount = getResponseCount(formResponses);
 
+  const handleSubmitClick = async () => {
+    setSubmitStatus(SubmitStatus.Submitting);
+    await submitResponses(formResponses, age || -1);
+    setSubmitStatus(SubmitStatus.Submitted);
+  }
   return (
     <div className={style['form-submit']}>
       <h1>Submit Your Ratings</h1>
 
       <div className={style['submit-box']}>
-        <h4>
-          You rated {responseCount} prayer{responseCount !== 1 && 's'}
-        </h4>
-        <input 
-          placeholder="(Optional) Age" 
-          type="number" 
-          onChange={e => setAge(parseInt(e.target.value))} 
-          min={0}
-        />
-        <button onClick={() => submitResponses(formResponses, age || -1)}>Submit</button>
+        {
+          submitStatus === SubmitStatus.NotSubmitted &&
+          (
+            <div>
+              <h4>
+                You rated {responseCount} prayer{responseCount !== 1 && 's'}
+              </h4>
+              <input
+                placeholder="(Optional) Age"
+                type="number"
+                onChange={e => setAge(parseInt(e.target.value))}
+                min={0}
+              />
+              <button onClick={handleSubmitClick}>Submit</button>
+            </div>
+          )
+        }
+
+        {
+          submitStatus === SubmitStatus.Submitting &&
+          (
+            <div>
+              <Loading />
+            </div>
+          )
+        }
+
+        {
+          submitStatus === SubmitStatus.Submitted &&
+          (
+            <div className={style['submitted']}>
+              <i className="fa fa-check" />
+              Submitted!
+            </div>
+          )
+        }
+
       </div>
 
     </div>
