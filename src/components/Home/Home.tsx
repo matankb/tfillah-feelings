4a8877e7f60c7f9c030e31df52fa78ab7648f66b
Matan Kotler-Berkowitz <205matan@gmail.com>
2020-08-25 23:29:13 -0400

    Fix Home header styling bug

--- src/components/Home/Home.tsx
+++ src/components/Home/Home.tsx
@@ -4,18 +4,16 @@ import SectionButton from '../SectionButton/SectionButton';
 
 import style from './home.module.css'
 
-const Home = () => {
-  return (
-    <div className={style['home-wrap']}>
-      <h1>T'Feeling</h1>
-      <div className={style['section-buttons']}>
-        <SectionButton name="Check In" icon="fas fa-tasks" color="cornflowerblue" linkTo="/check-in" />
-        <SectionButton name="Siddur" icon="fas fa-book-open" color="darkorange" linkTo="/siddur" />
-        <SectionButton name="Rate Prayers" icon="fas fa-edit" color="#e26f6f" linkTo="/rate" />
-        <SectionButton name="View Data" icon="fas fa-database" color="#4ed88b" linkTo="/data" />
-      </div>
+const Home = () => (
+  <div>
+    <h1 className={style['app-title']}>T'Feeling</h1>
+    <div className={style['section-buttons']}>
+      <SectionButton name="Check In" icon="fas fa-tasks" color="cornflowerblue" linkTo="/check-in" />
+      <SectionButton name="Siddur" icon="fas fa-book-open" color="darkorange" linkTo="/siddur" />
+      <SectionButton name="Rate Prayers" icon="fas fa-edit" color="#e26f6f" linkTo="/rate" />
+      <SectionButton name="View Data" icon="fas fa-database" color="#4ed88b" linkTo="/data" />
     </div>
-  )
-}
+  </div>
+)
 
 export default Home;
