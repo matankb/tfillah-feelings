5cea01ea6bb753de97d99545e956db59c846eb30
Matan Kotler-Berkowitz <205matan@gmail.com>
2020-11-20 13:20:57 -0500

    Add analytics

--- src/index.tsx
+++ src/index.tsx
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 
 import firebase from 'firebase/app';
 import 'firebase/firestore';
+import 'firebase/analytics';
 import { BrowserRouter } from 'react-router-dom';
 
 import App from './components/App/App';
@@ -15,9 +16,11 @@ firebase.initializeApp({
   projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
   storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
   messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.REACT_APPFIREBASE_APP_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
   measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID,
 });
+
+firebase.analytics();
 
 ReactDOM.render(
   <React.StrictMode>
